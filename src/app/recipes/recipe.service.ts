741a151cd7f2b26cf7030e71011a6196470c16e1
Lino John <b4lino@gmail.com>
2019-03-27 10:11:36 +0530

    services implemented

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,10 @@
+import { Recipe } from "./recipe.model";
+export class RecipeService {
+    private recipes:Recipe[] = [
+        new Recipe ("Recipe1","Description1","https://media2.s-nbcnews.com/j/newscms/2018_35/1363730/rachel-hollis-chicken-fingers-today-main-180828_b9b2a726ec8654e3f9f7435ce26588fb.today-inline-large.jpg"),
+        new Recipe ("Recipe2","Description2","https://media2.s-nbcnews.com/j/newscms/2018_35/1363730/rachel-hollis-chicken-fingers-today-main-180828_b9b2a726ec8654e3f9f7435ce26588fb.today-inline-large.jpg")
+    ];
+    getRecipes() {
+        return this.recipes.slice();
+    }
+}
--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { Recipe } from './recipes-item/recipe.model';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipes-list',
@@ -8,17 +9,14 @@ import { Recipe } from './recipes-item/recipe.model';
 })
 export class RecipesListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  @Output() recipes:Recipe[] = [
-    new Recipe ("Recipe1","Description1","https://media2.s-nbcnews.com/j/newscms/2018_35/1363730/rachel-hollis-chicken-fingers-today-main-180828_b9b2a726ec8654e3f9f7435ce26588fb.today-inline-large.jpg"),
-    new Recipe ("Recipe2","Description2","https://media2.s-nbcnews.com/j/newscms/2018_35/1363730/rachel-hollis-chicken-fingers-today-main-180828_b9b2a726ec8654e3f9f7435ce26588fb.today-inline-large.jpg")
-  ];
-  
+  recipes: Recipe[];
   OnSelectRecipe(selectedRecipe:Recipe) {
     this.recipeWasSelected.emit(selectedRecipe);
   }
-  constructor() { }
+  constructor(private recipeService : RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
 }
