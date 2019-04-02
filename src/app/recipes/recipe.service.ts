f2aa29d3d4a673bbae8c442dd75f115125cf1357
Lino John <b4lino@gmail.com>
2019-04-02 17:15:46 +0530

    Ingredient add to shopping list.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,29 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
-
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes:Recipe[] = [
-        new Recipe ("Recipe1","Description1","https://media2.s-nbcnews.com/j/newscms/2018_35/1363730/rachel-hollis-chicken-fingers-today-main-180828_b9b2a726ec8654e3f9f7435ce26588fb.today-inline-large.jpg"),
-        new Recipe ("Recipe2","Description2","https://media2.s-nbcnews.com/j/newscms/2018_35/1363730/rachel-hollis-chicken-fingers-today-main-180828_b9b2a726ec8654e3f9f7435ce26588fb.today-inline-large.jpg")
+        new Recipe ("Pork-Schnitzel",
+                    "Pork schnitzel is an easy recipe and it’s perfect for busy weeknights",
+                    "https://natashaskitchen.com/wp-content/uploads/2016/02/Pork-Schnitzel-Recipe-7-600x900.jpg",
+                    [new Ingredient ('boneless pork chops',1),
+                     new Ingredient ('eggs',3)]),
+        new Recipe ("Ground chicken burger",
+                    "This ground chicken burger recipe is juicy, moist, and flavorful! No more dry chicken, and you can have these burgers on the table in half an hour.",
+                    "https://www.saltandlavender.com/wp-content/uploads/2016/05/herbchickenburger1-680x1024.jpg",
+                    [new Ingredient ('Ground chicken breast',1),
+                     new Ingredient ('clove garlic minced',3)])
     ];
     getRecipes() {
         return this.recipes.slice();
     }
+    constructor(private shoppingListService: ShoppingListService) {
+
+    }
+    addIngredientsToShoppingList(ingredients : Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
+    } 
 }
