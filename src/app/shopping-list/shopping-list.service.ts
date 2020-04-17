527f7a99388d82511fefa2c8b16c1f521999691a
Lino-john <b4lino@gmail.com>
2020-04-17 19:19:01 +0530

    Eventemitter replaced with subject

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,10 @@
 import { Ingredient } from "../shared/ingredient.model";
-import { EventEmitter, Injectable } from "@angular/core";
-@Injectable()
+// import { EventEmitter, Injectable } from "@angular/core";
+// @Injectable()
+import { Subject } from 'rxjs';
+
 export class ShoppingListService {
-    ingredientsChanged = new EventEmitter<Ingredient[]>()
+    ingredientsChanged = new Subject<Ingredient[]>()
     private ingredients = [
         new Ingredient ("Apple", 5),
         new Ingredient ("Tomatoes", 10)
@@ -12,10 +14,10 @@ export class ShoppingListService {
     }
     addIngredient(ingredient : Ingredient) {
         this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
     addIngredients(ingredients : Ingredient[]) {
         this.ingredients.push(...ingredients);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 }
